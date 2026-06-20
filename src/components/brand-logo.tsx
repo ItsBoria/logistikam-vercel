0b9f828623eb91adab1c44dbf459a10b0c82a823
ItsBoria <davidpanasik@hotmail.com>
2026-06-20 18:54:39 +0300

    use repository-owned logo

--- src/components/brand-logo.tsx
+++ src/components/brand-logo.tsx
@@ -1,10 +1,9 @@
-import logoAsset from "@/assets/logikam-logo.png.asset.json";
 import { cn } from "@/lib/utils";
 
 export function BrandLogo({ className, size = 64 }: { className?: string; size?: number }) {
   return (
     <img
-      src={logoAsset.url}
+      src="/logikam-logo.svg"
       alt="Logikam"
       width={size}
       height={size}
